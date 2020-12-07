011f2407c711852e1f1d19f77750b57fd5d83d29
Chris Cooley <chriscooley1984@yahoo.com>
2020-12-07 15:45:47 -0700

    added table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+##Description
+${data.description}
 
+##Table of Contents
+* [Installation](#install)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [License](#license)
+* [Contact](#contact)
+
+## Installation
+${data.install}
+## Usage
+${data.usage}
+## Contributors
+${data.contributors}
+## Tests
+${data.tests}
+## License
+${data.license}
+## Contact
+Github Profile: [${data.githubURL}](${data.githubURL})
+Email: [${data.email}](mailto:${data.email})
 `;
 }
 
