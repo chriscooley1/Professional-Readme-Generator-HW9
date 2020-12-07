701ad76b628a340d58cb8b872099d3452fad8278
Chris Cooley <chriscooley1984@yahoo.com>
2020-12-07 15:33:51 -0700

    added questions and answers

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+const questions = inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "what is your project name?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Project description: ",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["Apache 2.0", "MIT", "GNU GPL v3.0"],
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use the project?",
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "How do you install this project?",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who contributed to this project?",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "List any tests for this project:",
+    },
+    {
+        type: "input",
+        name: "githubURL",
+        message: "GitHub username: ",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Email address: ",
+    },
+])
+.then((answers) => {
+    fs.writeFileSync("readME.md", generateMarkdown({...answers}), (err) => {
+        if (err) throw err;
+        console.log("Success!")
+    })
+});
